ea3f87769ce186bdb5a6c8b91f43ead6d88aee4a
Anpromole <anpromole@gmail.com>
2026-04-27 11:37:30 +0800

    Add dist directory creation step

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    pyappify_lib::run();
 }
