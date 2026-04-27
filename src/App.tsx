49e9e79d8b81be274dc19654f8b651f24c7b972c
Anpromole <anpromole@gmail.com>
2026-04-27 11:26:35 +0800

    Simplify: remove React and MUI dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import { useState } from "react";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
       <h1>pyappify</h1>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
+      <p>MediaSpider Package Builder</p>
     </div>
   );
 }
